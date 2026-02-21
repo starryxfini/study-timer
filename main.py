b6619ede7c137d891b06386ec2de26d7be75bd15
fini <jsabew_spam@icloud.com>
2026-02-21 22:48:45 +0100

    changed stuff so its using less cpu or smth

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from datetime import datetime, timedelta
 import time
 
 print("⋆｡study timer°✩")
 
-minutes = int(input("how many minutes do you want to study? "))
 
+minutes = int(input("how many minutes do you want to study? "))
 seconds = minutes * 60
 
-while seconds > 0:
+end_time = datetime.now() + timedelta(seconds = seconds)
+
+while end_time > datetime.now():
     mins = seconds // 60
     secs = seconds % 60
     print(f"time left: {mins:02d}:{secs:02d}", end="\r")
     time.sleep(1)
     seconds -= 1
+    
 
 print("\n  ˖.𖥔 ݁ time is up˖ ⊹good job! ࣪ ˖ ")
